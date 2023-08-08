7409b22be62da646d47b5ae231e081d7d2270818
Evgenii Sopov <mrseakg@gmail.com>
2023-08-08 10:03:46 +0700

    Updated cmake modules

--- source/plugins/Sea5kg/StoneGenerator_editorplugin/cmake/modules/FindEditor.cmake
+++ source/plugins/Sea5kg/StoneGenerator_editorplugin/cmake/modules/FindEditor.cmake
@@ -5,8 +5,6 @@
 #  UnigineExt::Editor - library target.
 #  Editor_FOUND - library is found.
 #  Editor_LIBRARY - library path.
-#  Editor_INCLUDE_DIR - API include path.
-
 
 if (TARGET Unigine::Editor)
 	set(Editor_FOUND TRUE)
@@ -21,58 +19,45 @@ string(APPEND name "_x64")
 
 find_library(Editor_LIBRARY_RELEASE
 	NAMES ${name}
-	PATHS ${PROJECT_SOURCE_DIR}/../../../../bin ${PROJECT_SOURCE_DIR}/../../../../lib
+	PATHS ${UNIGINE_BIN_DIR} ${UNIGINE_LIB_DIR}
 	NO_DEFAULT_PATH
 	)
 
 find_library(Editor_LIBRARY_DEBUG
 	NAMES ${name}d
-	PATHS ${PROJECT_SOURCE_DIR}/../../../../bin ${PROJECT_SOURCE_DIR}/../../../../lib
+	PATHS ${UNIGINE_BIN_DIR} ${UNIGINE_LIB_DIR}
 	NO_DEFAULT_PATH
 	)
 
 include(SelectLibraryConfigurations)
 select_library_configurations(Editor)
 
-message(STATUS ${PROJECT_SOURCE_DIR})
-find_path(Editor_INCLUDE_DIR
-	NAMES "UniginePlugin.h"
-	PATHS ${PROJECT_SOURCE_DIR}/../../../../include/editor
-	NO_DEFAULT_PATH
-	)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Editor
 	FOUND_VAR Editor_FOUND
 	REQUIRED_VARS
 		Editor_LIBRARY
-		Editor_INCLUDE_DIR
+		UNIGINE_INCLUDE_DIR
 	)
 
 if(Editor_FOUND)
 	add_library(Unigine::Editor UNKNOWN IMPORTED)
-	set_target_properties(Unigine::Editor PROPERTIES
-		INTERFACE_INCLUDE_DIRECTORIES ${Editor_INCLUDE_DIR}
-		INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${Editor_INCLUDE_DIR}
+
+	set_target_properties(Unigine::Editor
+		PROPERTIES
+		INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${UNIGINE_INCLUDE_DIR}
+		INTERFACE_INCLUDE_DIRECTORIES ${UNIGINE_INCLUDE_DIR}
 		)
-	set_property(TARGET Unigine::Editor PROPERTY
+	set_property(TARGET Unigine::Editor
+		PROPERTY
 		INTERFACE_COMPILE_DEFINITIONS $<$<BOOL:${UNIGINE_DOUBLE}>:UNIGINE_DOUBLE>
 		)
 
-	if (Editor_LIBRARY_RELEASE)
-		set_target_properties(Unigine::Editor PROPERTIES
-			IMPORTED_LOCATION_RELEASE ${Editor_LIBRARY_RELEASE}
-			IMPORTED_LOCATION_MINSIZEREL ${Editor_LIBRARY_RELEASE}
-			)
-	endif()
-
-	if (Editor_LIBRARY_DEBUG)
-		set_target_properties(Unigine::Editor PROPERTIES
-			IMPORTED_LOCATION_DEBUG ${Editor_LIBRARY_DEBUG}
-			)
-		set_target_properties(Unigine::Editor PROPERTIES
-			IMPORTED_LOCATION_RELWITHDEBINFO ${Editor_LIBRARY_DEBUG}
-			)
-	endif()
+	set_target_properties(Unigine::Editor
+		PROPERTIES
+		IMPORTED_LOCATION ${Editor_LIBRARY_RELEASE}
+		IMPORTED_LOCATION_DEBUG ${Editor_LIBRARY_DEBUG}
+		IMPORTED_LOCATION_RELEASE ${Editor_LIBRARY_RELEASE}
+		)
 
 endif()
--- source/plugins/Sea5kg/StoneGenerator_editorplugin/cmake/modules/FindEngine.cmake
+++ source/plugins/Sea5kg/StoneGenerator_editorplugin/cmake/modules/FindEngine.cmake
@@ -5,8 +5,6 @@
 #  UnigineExt::Engine - library target.
 #  Engine_FOUND - library is found.
 #  Engine_LIBRARY - library path.
-#  Engine_INCLUDE_DIR - API include path.
-
 
 if (TARGET Unigine::Engine)
 	set(Engine_FOUND TRUE)
@@ -21,59 +19,45 @@ string(APPEND name "_x64")
 
 find_library(Engine_LIBRARY_RELEASE
 	NAMES ${name}
-	PATHS ${PROJECT_SOURCE_DIR}/../../../../bin ${PROJECT_SOURCE_DIR}/../../../../lib
+	PATHS ${UNIGINE_BIN_DIR} ${UNIGINE_LIB_DIR}
 	NO_DEFAULT_PATH
 	)
-
 find_library(Engine_LIBRARY_DEBUG
 	NAMES ${name}d
-	PATHS ${PROJECT_SOURCE_DIR}/../../../../bin ${PROJECT_SOURCE_DIR}/../../../../lib
+	PATHS ${UNIGINE_BIN_DIR} ${UNIGINE_LIB_DIR}
 	NO_DEFAULT_PATH
 	)
 
 include(SelectLibraryConfigurations)
 select_library_configurations(Engine)
 
-find_path(Engine_INCLUDE_DIR
-	NAMES "Unigine.h"
-	PATHS ${PROJECT_SOURCE_DIR}/../../../../include
-	NO_DEFAULT_PATH
-	)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Engine
 	FOUND_VAR Engine_FOUND
 	REQUIRED_VARS
 		Engine_LIBRARY
-		Engine_INCLUDE_DIR
+		UNIGINE_INCLUDE_DIR
 	)
 
 if(Engine_FOUND)
 	add_library(Unigine::Engine UNKNOWN IMPORTED)
-	set_target_properties(Unigine::Engine PROPERTIES
-		INTERFACE_INCLUDE_DIRECTORIES ${Engine_INCLUDE_DIR}
-		INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${Engine_INCLUDE_DIR}
+
+	set_target_properties(Unigine::Engine
+		PROPERTIES
+		INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${UNIGINE_INCLUDE_DIR}
+		INTERFACE_INCLUDE_DIRECTORIES ${UNIGINE_INCLUDE_DIR}
 		)
-	set_property(TARGET Unigine::Engine PROPERTY
+
+	set_property(TARGET Unigine::Engine
+		PROPERTY
 		INTERFACE_COMPILE_DEFINITIONS $<$<BOOL:${UNIGINE_DOUBLE}>:UNIGINE_DOUBLE>
 		)
 
-	if (Engine_LIBRARY_RELEASE)
-		set_target_properties(Unigine::Engine PROPERTIES
-			IMPORTED_LOCATION_RELEASE ${Engine_LIBRARY_RELEASE}
-			)
-		set_target_properties(Unigine::Engine PROPERTIES
-			IMPORTED_LOCATION_MINSIZEREL ${Engine_LIBRARY_RELEASE}
-			)
-	endif()
-
-	if (Engine_LIBRARY_DEBUG)
-		set_target_properties(Unigine::Engine PROPERTIES
-			IMPORTED_LOCATION_DEBUG ${Engine_LIBRARY_DEBUG}
-			)
-		set_target_properties(Unigine::Engine PROPERTIES
-			IMPORTED_LOCATION_RELWITHDEBINFO ${Engine_LIBRARY_DEBUG}
-			)
-	endif()
+	set_target_properties(Unigine::Engine
+		PROPERTIES
+		IMPORTED_LOCATION ${Engine_LIBRARY_RELEASE}
+		IMPORTED_LOCATION_DEBUG ${Engine_LIBRARY_DEBUG}
+		IMPORTED_LOCATION_RELEASE ${Engine_LIBRARY_RELEASE}
+		)
 
 endif()
