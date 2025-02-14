857489cd771997c1abd885afad805d1d8edd74a5
Evgenii Sopov <mrseakg@gmail.com>
2025-02-15 02:14:51 +0700

    Updated editor launchers

--- launch_editor.sh
+++ launch_editor.sh
@@ -2,7 +2,7 @@ SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
 APP="$SCRIPT_DIR/bin/Editor_x64"
 export LD_LIBRARY_PATH="$SCRIPT_DIR/bin:$LD_LIBRARY_PATH"
 if [ -f "$APP" ]; then
-	"$APP" -video_app auto -video_refresh 0 -video_debug 0 -main_window_size 2560 1440 -main_window_resizable 1 -main_window_fullscreen 0 -render_vsync 0 -video_offscreen 0 -sound_app auto -data_path ../data/ -extern_plugin UnigineFbxImporter,UnigineGLTFImporter,UnigineFbxExporter,UnigineUsdExchanger -console_command "config_autosave 1 && world_load \"test\""
+	"$APP" -video_app auto -video_adapter 0 -video_refresh 0 -video_debug 2 -main_window_size 2560 1440 -main_window_resizable 1 -main_window_fullscreen 0 -render_vsync 0 -video_offscreen 0 -sound_app auto -data_path ../data/ -extern_plugin UnigineFbxImporter,UnigineGLTFImporter,UnigineFbxExporter,UnigineUsdExchanger -console_command "config_autosave 1 && world_load \"test\""
 else
 	echo "$APP not found"
 fi
