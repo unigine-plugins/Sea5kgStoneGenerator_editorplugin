6a8cfc488f515025fee74d478c6165fc540ce1d6
Evgenii Sopov <mrseakg@gmail.com>
2025-11-09 14:58:16 +0700

    Redesign ./build_plugin.py to ./pm build

--- libpm/command_build.py
+++ libpm/command_build.py
@@ -27,7 +27,9 @@
 """ Comamnd for build plugin """
 
 import logging
+import sys
 from .pm_config import PmConfig
+from .utils_files import UtilsFiles
 
 logging.basicConfig()
 
@@ -63,6 +65,25 @@ class CommandBuild:
         """ run """
         self.__log.info("OK: Start building...")
         self.__log.info("Root directory: %s", self.__config.get_root_dir())
+
+        CMAKE_GEN = ""
+        IS_LINUX = False
+        IS_WINDOWS = False
+        if UtilsFiles.is_linux():
+            _PLATFORM = "linux"
+            self.__log.info(" * Linux platform")
+            IS_LINUX = True
+        elif UtilsFiles.is_windows():
+            _PLATFORM = "windows"
+            self.__log.info(" * Windows platform")
+            # TODO check ver of sdk or look in file settings
+            CMAKE_GEN = " -G \"Visual Studio 17 2022\" -A x64"
+            IS_WINDOWS = True
+        else:
+            sys.exit("Unknown platform")
+
+        _junk_path = "../../../../junk/Sea5kgStoneGenerator_editorplugin"
+
         # TODO check binaries and can by add to bin directory
         # TODO upgrade include/Unigine.h
         # TODO detect in docker or not
@@ -82,3 +103,89 @@ class CommandBuild:
         # if _exit_code == 0:
         #     self.__log.info("OK: Looks everything fine...")
         # sys.exit(_exit_code)
+
+
+
+
+HAS_FLOAT = False
+HAS_DOUBLE = False
+
+for _file in os.listdir("./bin"):
+    if _file == "libUnigine_x64.so":
+        print(" * Found engine (float)")
+        HAS_FLOAT = True
+    if _file == "libUnigine_double_x64.so":
+        print(" * Found double (engine)")
+        HAS_DOUBLE = True
+
+build_commands = []
+
+if HAS_FLOAT:
+    _junk_rel = _JUNK_PATH + "_release_float_" + _PLATFORM
+    _junk_dbg = _JUNK_PATH + "_release_debug_" + _PLATFORM
+    build_commands.extend([
+        # build float
+        {
+            "name": "cmake configure release (float)",
+            "command": "cmake -H. -B" + _junk_rel + " " + CMAKE_GEN + " \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+            -DCMAKE_INSTALL_PREFIX=../../../../bin",
+        },
+        {
+            "name": "cmake build release (float)",
+            "command": "cmake --build " + _junk_rel + " --parallel 8 --config Release",
+        },
+        {
+            "name": "cmake configure debug (float)",
+            "command": "cmake -H. -B" + _junk_dbg + " " + CMAKE_GEN + " \
+            -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+            -DCMAKE_INSTALL_PREFIX=../../../../bin",
+        },
+        {
+            "name": "cmake build debug (float)",
+            "command": "cmake --build " + _junk_dbg + " --parallel 8 --config RelWithDebInfo",
+        },
+    ])
+
+if HAS_DOUBLE:
+    _junk_rel = _JUNK_PATH + "_release_double_" + _PLATFORM
+    _junk_dbg = _JUNK_PATH + "_debug_double_" + _PLATFORM
+    build_commands.extend([
+        {
+            "name": "cmake configure release (double)",
+            "command": "cmake -H. -B" + _junk_rel + " " + CMAKE_GEN + " \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+            -DUNIGINE_DOUBLE=ON \
+            -DCMAKE_INSTALL_PREFIX=../../../../bin",
+        },
+        {
+            "name": "cmake build release (double)",
+            "command": "cmake --build " + _junk_rel + " --parallel 8 --config Release",
+        },
+        {
+            "name": "cmake configure debug (double)",
+            "command": "cmake -H. -B" + _junk_dbg + " " + CMAKE_GEN + " \
+            -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+            -DUNIGINE_DOUBLE=ON \
+            -DCMAKE_INSTALL_PREFIX=../bin",
+        },
+        {
+            "name": "cmake build debug (double)",
+            "command": "cmake --build " + _junk_dbg + " --parallel 8 --config RelWithDebInfo",
+        },
+    ])
+
+os.chdir("source/plugins/Sea5kg/StoneGenerator_editorplugin")
+
+for _cmd in build_commands:
+    print("Starting... " + _cmd["name"])
+    print("command: " + _cmd["command"])
+    ret = os.system(_cmd["command"])
+    if ret != 0:
+        print("Failed... " + _cmd["name"])
+        sys.exit(-1)
+    print("Done. " + _cmd["name"] + "\n\n-----\n\n")
